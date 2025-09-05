0699b23361725418197ba6b6ca513ffe8461ecd8
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-09-05 14:49:39 +0000

    added pagination and global token authentication

--- capstone_project/capstone_project/settings.py
+++ capstone_project/capstone_project/settings.py
@@ -143,7 +143,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# settings.py
+
 REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
