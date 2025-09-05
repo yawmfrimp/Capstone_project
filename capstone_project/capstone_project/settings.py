e50950172037f1fa19185105382a638899151afe
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-09-05 13:55:29 +0000

    added pagination and updated installed apps

--- capstone_project/capstone_project/settings.py
+++ capstone_project/capstone_project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'movie_review.apps.MovieReviewConfig'    
 ]
 
@@ -141,3 +142,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
