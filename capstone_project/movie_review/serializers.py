ddfd2b77545a2f2b17f4df68fe1b74c81f70fa65
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-09-05 12:13:14 +0000

    Added register serializer

--- capstone_project/movie_review/serializers.py
+++ capstone_project/movie_review/serializers.py
@@ -49,3 +49,21 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'role', 'joined_date']
         read_only_fields = ['id', 'joined_date']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    '''This serializer handles the registeration of new users'''
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username','email', 'password', 'role']
+        
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            role='member'
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
