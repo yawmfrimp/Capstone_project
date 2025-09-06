fb8515db0a3ac302f1dc036c26243c144af5951e
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-09-06 13:39:45 +0000

    added django_extensions to settings

--- capstone_project/capstone_project/settings.py
+++ capstone_project/capstone_project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
     'movie_review.apps.MovieReviewConfig'    
